8990bb9a2b42d485ab2c3603c4159c14a32ad944
TeamWolfyta <kieran@wolfyta.dev>
2024-11-28 11:06:38 +0000

    feat(flake/templates): Add TypeScript and Rust templates

---
+++ templates/rust/flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      devenv,
+      ...
+    }@inputs:
+    let
+      forEachSystem =
+        function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            (_: {
+              languages = {
+                nix.enable = true;
+                rust.enable = true;
+              };
+
+              pre-commit.hooks = {
+                clippy.enable = true;
+                commitlint-rs = {
+                  enable = true;
+                  entry = "commitlint --edit";
+                  language = "rust";
+                  package = pkgs.commitlint-rs;
+                  pass_filenames = false;
+                  stages = [ "commit-msg" ];
+                };
+                deadnix.enable = true;
+                rustfmt.enable = true;
+                statix.enable = true;
+                nixfmt-rfc-style.enable = true;
+              };
+            })
+          ];
+        };
+      });
+    };
+}
---
+++ templates/typescript/flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      devenv,
+      ...
+    }@inputs:
+    let
+      forEachSystem =
+        function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            (_: {
+              languages = {
+                javascript = {
+                  enable = true;
+                  bun.enable = true;
+                  npm.enable = true;
+                  pnpm.enable = true;
+                };
+                nix.enable = true;
+                typescript.enable = true;
+              };
+
+              pre-commit.hooks = {
+                biome.enable = true;
+                commitlint-rs = {
+                  enable = true;
+                  entry = "commitlint --edit";
+                  language = "rust";
+                  package = pkgs.commitlint-rs;
+                  pass_filenames = false;
+                  stages = [ "commit-msg" ];
+                };
+                deadnix.enable = true;
+                statix.enable = true;
+                nixfmt-rfc-style.enable = true;
+              };
+            })
+          ];
+        };
+      });
+    };
+}
