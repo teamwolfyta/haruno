04ae817788b049ed6e283cd1320a29751e4c4cd9
TeamWolfyta <kieran@wolfyta.dev>
2025-03-16 01:21:38 +0000

    refactor(repl): simplify flake inheritance

--- repl.nix
+++ repl.nix
@@ -1,12 +1,5 @@
 let
-  inherit (builtins) attrNames getFlake listToAttrs;
+  inherit (builtins) getFlake;
   flake = getFlake (toString ./.);
-  names = attrNames flake;
-  inherited = listToAttrs (
-    map (name: {
-      inherit name;
-      value = flake.${name};
-    }) names
-  );
 in
-inherited // { inherit flake; }
+flake // { inherit flake; }
