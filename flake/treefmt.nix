ff7852b645fbb43cd5b481421225679cdfc5424e
TeamWolfyta <kieran@wolfyta.dev>
2025-04-02 00:33:00 +0100

    feat: enable additional formatting tools in treefmt-nix

--- flake/treefmt.nix
+++ flake/treefmt.nix
@@ -6,9 +6,13 @@
   perSystem = _: {
     treefmt = {
       programs = {
+        actionlint.enable = true;
         alejandra.enable = true;
         deadnix.enable = true;
+        jsonfmt.enable = true;
+        mdformat.enable = true;
         statix.enable = true;
+        yamlfmt.enable = true;
       };
     };
   };
