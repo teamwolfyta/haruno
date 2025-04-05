1fa862f4452e22616999383fc4511d4a1c4555c5
TeamWolfyta <kieran@wolfyta.dev>
2025-04-05 11:04:33 +0100

    style: restructure inputs

--- flake.nix
+++ flake.nix
@@ -1,29 +1,16 @@
 {
   inputs = {
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
-
-    flake-parts-builder = {
-      url = "github:tsandrini/flake-parts-builder";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts-builder.url = "github:tsandrini/flake-parts-builder";
 
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-
-    systems = {
-      url = "github:nix-systems/default";
-    };
-
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   nixConfig = {
