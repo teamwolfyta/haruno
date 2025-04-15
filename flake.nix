92810c65febb1087531c5dbe9b2e9fb984e932f4
TeamWolfyta <kieran@wolfyta.dev>
2025-04-15 15:21:19 +0100

    refactor: remove git-hooks and treefmt-nix

--- flake.nix
+++ flake.nix
@@ -8,13 +8,9 @@
   nixConfig = {
     extra-substituters = [
       "https://haruno.cachix.org"
-      "https://numtide.cachix.org"
-      "https://pre-commit-hooks.cachix.org/"
     ];
     extra-trusted-public-keys = [
       "haruno.cachix.org-1:ujqqGR45bFhbAaDWg9owXuG71PZnLvynF/MjvRxrK5k="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
     ];
   };
 
--- flake/partitions/development/flake.nix
+++ flake/partitions/development/flake.nix
@@ -1,12 +1,6 @@
 {
   inputs = {
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = _: {};
